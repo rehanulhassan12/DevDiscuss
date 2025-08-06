4a28d6c65070ee7bac9d11600a850aa37e8e4359
Rehanulhassan12 <rehanreigns123@gmail.com>
2025-08-06 18:55:29 +0500

    Fix page dynamic params import

--- src/app/(protected)/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/(protected)/questions/[quesId]/[quesName]/edit/page.tsx
@@ -4,14 +4,13 @@ import React from "react";
 import EditQuestion from "./EditQuestion";
 
 // Define the prop type explicitly
-type Props = {
-  params: {
-    quesId: string;
-    quesName: string;
-  };
+type PageProps = {
+  params: Promise<{ quesId: string; quesName: string }>;
 };
 
-export default async function Page({ params }: Props) {
-  const question = await databases.getDocument(db, questionCollection, params.quesId);
+export default async function Page({ params }: PageProps) {
+  const { quesId } = await params;
+  const question = await databases.getDocument(db, questionCollection, quesId);
   return <EditQuestion question={question} />;
 }
+
