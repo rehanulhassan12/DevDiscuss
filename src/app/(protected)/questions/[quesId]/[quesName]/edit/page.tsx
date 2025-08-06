b7fe12dc58fa2dce5a38f80a0283fe839df975e6
Rehanulhassan12 <rehanreigns123@gmail.com>
2025-08-06 18:28:15 +0500

    Fix Edit Question Page

--- src/app/(protected)/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/(protected)/questions/[quesId]/[quesName]/edit/page.tsx
@@ -1,13 +1,17 @@
 import { db, questionCollection } from "@/models/name";
 import { databases } from "@/models/client/config";
-import React from 'react'
+import React from "react";
 import EditQuestion from "./EditQuestion";
-async function page({ params }: { params: { quesId: string, quesName: string } }) {
+
+// Define the prop type explicitly
+type Props = {
+  params: {
+    quesId: string;
+    quesName: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
   const question = await databases.getDocument(db, questionCollection, params.quesId);
-
-
   return <EditQuestion question={question} />;
-
 }
-
-export default page
